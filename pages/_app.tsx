585c38212571a5cccd9330dcfb148c0bbbdb6a1b
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2022-07-11 17:49:46 +0530

    navbar comp as default header for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import NavBar from '../components/navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div>
+			<NavBar />
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
